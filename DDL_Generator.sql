484eaadba5cbce3f0a0bcf91d20dba2de24ce5f6
jeffrose20 <jeffrose20@gmail.com>
2018-01-07 21:47:43 -0500

    First Commit
    The SPROC has basic functionality.  It can return the CREATE statement but only contains the column names and table name.

---
+++ DDL_Generator.sql
@@ -0,0 +1,72 @@
+--SP_DDL_Generator 'AWBuildVersion'
+
+IF EXISTS(SELECT * FROM sys.procedures WHERE Name like 'SP_DDL_Generator')
+	DROP PROCEDURE [dbo].[SP_DDL_Generator]
+GO
+CREATE PROCEDURE [dbo].[SP_DDL_Generator]
+(
+	@ObjectName SYSNAME
+)
+AS
+
+SET NOCOUNT ON
+
+BEGIN TRY
+	--This is the string containing the DDL info the we will print to the console
+	DECLARE @DDL_String NVARCHAR(MAX) = ''
+
+	SET @DDL_String = 
+		'    USE ' + CONVERT(NVARCHAR, DB_NAME()) + ' 
+	GO
+
+	CREATE '
+
+	SELECT 
+		@DDL_String = @DDL_String + 
+		CASE 
+			WHEN AO.Type = 'V'
+			THEN 'VIEW'
+			WHEN AO.Type = 'U'
+			THEN 'TABLE '
+		END
+
+		+ '[' + S.Name + '].[' + AO.Name + ']
+		('
+	+
+		STUFF(Columns.Name, LEN(Columns.Name), 1, '')
+		--Columns.Name
+	+
+	'
+		)'
+	FROM
+		sys.all_objects AS AO
+		INNER JOIN
+			sys.schemas AS S
+				ON S.Schema_ID = AO.Schema_ID
+		CROSS APPLY
+			(
+
+				SELECT
+					(
+						SELECT	
+							CHAR(10) + CHAR(9) + CHAR(9) + CHAR(9) + '[' + Name + '],'
+						FROM
+							sys.all_columns AS AC
+						WHERE
+							AC.object_ID = AO.Object_ID
+						FOR XML PATH('')
+					) AS Name
+			) AS Columns
+		WHERE
+			AO.Name = @ObjectName
+
+
+		PRINT @DDL_String
+END TRY
+
+BEGIN CATCH
+	DECLARE @ErrorMsg NVARCHAR(2048) = ERROR_MESSAGE()
+	RAISERROR(@ErrorMsg, 16, 1)
+END CATCH
+
+SET NOCOUNT OFF
